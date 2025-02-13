74af63f16e27a36e4839831f4f91fb8438a3bfa2
NishalyV <nishaly.venkatesan@elpaviation.com>
2025-02-13 15:47:10 +0530

    initial commit for profile module

---
+++ profile-app/projects/profile/src/lib/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profile-profile',
+  template: `
+    <p>
+      profile works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProfileComponent {
+
+}
---
+++ profile-app/projects/profile/src/lib/profile.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ProfileComponent } from './profile.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProfileComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ProfileComponent
+  ]
+})
+export class ProfileModule { }
